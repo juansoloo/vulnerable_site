f691228ce8d447e1508acf45ab3acb8dea0a106d
Juan Hurtado <hurtadojuan93@yahoo.com>
2025-05-26 22:34:40 -0400

    made changes to port number in vite to interact with burpsuites proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { // configuration to proxy to burp VVVV
+    port: 8081,
+    host: '127.0.0.1'
+  }
 })
